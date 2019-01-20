80de91413849b1c74b6ddf4b00480e07522d8e96
John McKay <adenosine3p@gmail.com>
2019-01-20 16:43:44 +0000

    Fix logging, add header config

--- pyrelay-rspamd.py
+++ pyrelay-rspamd.py
@@ -7,12 +7,12 @@ import email
 import email.policy
 import requests
 import smtplib
+import logging
+import logging.handlers
+import socket
 from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import Message
-
-class LMTPController(Controller):
-    def factory(self):
-        return aiosmptd.smtp.SMTP(self.handler, enable_SMTPUTF8=self.enable_SMTPUTF8)
+from aiosmtpd.smtp import SMTP
 
 class Handler(Message):
     def prepare_message(self, session, envelope):
@@ -28,33 +28,50 @@ class Handler(Message):
         return message
 
     def handle_message(self, message):
-        global config
-        h = {'IP': '127.0.0.1'}
-        if 'rspamd_settings-id' in config['RELAY']:
-            h['Settings-ID'] = config['RELAY']['rspamd_settings-id']
         r = requests.post('http://localhost:11333/checkv2', data=message.as_bytes(),
-                headers = h)
+                headers = dict(config.items('HEADERS')))
         j = r.json()
-        message['DKIM-Signature'] = j['dkim-signature'].replace('\r', '').replace('\n', '')
+
+        if 'dkim-signature' not in j:
+            logging.warning('rspamd did not return a signature for message {}'.format(message.get('Message-ID', '(Message-ID not found)')))
+        else:
+            message['DKIM-Signature'] = j['dkim-signature'].replace('\r', '').replace('\n', '')
         with smtplib.SMTP('localhost', 2525) as s:
             s.send_message(message)
 
-
-syslog.openlog(logoption=syslog.LOG_PID, facility=syslog.LOG_MAIL)
-syslog.syslog("Starting pyrelay-rspamd")
-
-if len(sys.argv) < 2:
-    syslog.syslog(syslog.LOG_ERR, "No configuration file specified")
-    sys.exit(2)
+    async def handle_exception(self, err):
+        logging.error("Unable to relay message: {}".format(err))
+        return "554 Unable to relay mail"
 
 global config
+if len(sys.argv) < 2:
+    print("No configuration file specified", file=os.stderr)
+    sys.exit(2)
 config = configparser.ConfigParser()
 try:
     config.read_file(open(sys.argv[1]))
 except configparser.Error as err:
-    syslog.syslog(syslog.LOG_ERR, "Unable to parse configuration: {}".format(err))
+    print("Unable to parse configuration: {}".format(err), file=os.stderr)
     sys.exit(2)
-#controller = LMTPController(Handler(),hostname="localhost", port=8025)
+
+if 'LOG' in config:
+    logger = logging.getLogger()
+    handler = {'syslog': logging.handlers.SysLogHandler(facility='mail',address='/dev/log'),
+                'none': logging.NullHandler(),
+                'file': logging.handlers.TimedRotatingFileHandler(config['LOG'].get('file', 'pyrelay-rspamd.log'), when='midnight')
+              }.get(config['LOG'].get('type'), logging.StreamHandler())
+    if isinstance(handler, logging.handlers.SysLogHandler):
+        handler.ident = "pyrelay-rspamd: "
+    else:
+        fmt = logging.Formatter(fmt='{asctime}:{levelname}: {message}', style='{')
+        handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    logger.setLevel(config['LOG'].get('level', 'INFO'))
+else:
+    logging.basicConfig()
+
+logging.info("Starting pyrelay-rspamd")
+
 controller = Controller(Handler())
 controller.start()
 print("Hit enter to stop")
